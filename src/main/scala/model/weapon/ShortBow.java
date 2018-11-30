37fb8a64e3bd260b9f9ce90199cd7deee017e192
Alexandre <alexandrelebegue12@gmail.com>
2018-11-30 08:48:54 -0500

    implémentation des dommages sur great sword

--- src/main/scala/model/weapon/ShortBow.java
+++ src/main/scala/model/weapon/ShortBow.java
@@ -1,14 +1,29 @@
 package model.weapon;
 
+import model.Dice;
 import model.mob.Mob;
 import model.mob.WorgRider;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 public class ShortBow extends Weapon {
 
+    private Dice d6 = new Dice(6);
+
+
     public ShortBow(Mob mob) {
         super(mob);
+        name = "ShortBow";
+        precision = new ArrayList<Integer>();
+        Collections.addAll(precision, 4);
+        damage = 4;
+        range  = 20;
+    }
+
+    @Override
+    protected int calculDamage(){
+        return d6.getAThrow() * 3;
     }
 }
--- src/main/scala/model/weapon/ThrowingAxe.java
+++ src/main/scala/model/weapon/ThrowingAxe.java
@@ -1,15 +1,31 @@
 package model.weapon;
 
+import model.Dice;
 import model.mob.Mob;
 import model.mob.WarLord;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 public class ThrowingAxe extends Weapon {
 
+    private Dice d6 = new Dice(6);
+
     public ThrowingAxe(Mob mob) {
         super(mob);
-
+        name = "ThrowingAxe";
+        precision = new ArrayList<Integer>();
+        Collections.addAll(precision, 19);
+        damage = 5;
+        range  = 20;
     }
+
+    @Override
+    protected int calculDamage(){
+        return d6.getAThrow()+ damage;
+    }
+    
+
+
 }
