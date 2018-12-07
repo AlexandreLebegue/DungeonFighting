125752b0a56965b23d7095e000adbb932bf45599
rachelnoireau <rachel.noireau@uha.fr>
2018-12-07 08:42:30 -0500

    planetar

--- src/main/scala/model/mob/Planetar.java
+++ src/main/scala/model/mob/Planetar.java
@@ -3,15 +3,18 @@ package model.mob;
 import model.weapon.GreatSword;
 import model.weapon.LongBow;
 
-public class Planetar {
+public class Planetar extends Mob{
 
     public Planetar(){
-        this.name = "Solar";
+        this.name = "Planetar";
         this.health = 363;
         this.armor = 44;
         this.weapons.add(new GreatSword(this));
         this.weapons.add(new LongBow(this));
     }
 
+    public boolean haveToMove(){
+        return false;
+    }
 
 }
